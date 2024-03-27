808565d28c929e593bcb05992a2978eab41b1e7d
Stephy Raju Joseph <stephyrjoseph89@gmail.com>
2024-03-27 09:34:20 +0000

    second

--- express-mysql/index.js
+++ express-mysql/index.js
@@ -2,15 +2,23 @@ const express = require('express');
 const  hbs = require('hbs');
 const wax = require('wax-on');
 const {createConnection} = require('mysql2/promise');
+// const { Connection } = require('mysql2/typings/mysql/lib/Connection');
 require('dotenv').config();
 
 const app = express();
+
+
 
 //set up the view engine
 app.set('view engine','hbs');
 
 //enable static files
 app.use(express.static('public'));
+
+// enable the form processing
+app.use(express.urlencoded({
+    extended:false
+}))
 
 //wax-on(templte inheritance)
 wax.on(hbs.handlebars);
@@ -27,7 +35,7 @@ async function main(){
     app.get('/Users', async function(req,res){
         // the [] is known as array destucting
        let[Users]= await connection.execute(`
-       SELECT * FROM  Users
+       SELECT * FROM  Users 
        `);
        res.render('Users',{
         'Users':Users
@@ -36,7 +44,43 @@ async function main(){
     app.get('/create-Users', async function(req,res){
         res.render('create-Users')
     })
+    app.post('/create-Users', async function(req,res){
+        const{name,email,address,contact_no}=req.body;
+        const query= `INSERT INTO Users(name,email,address,contact_no)
+        VALUES ("${name}","${email}","${address}","${contact_no}");`
 
+        //res.send(query);
+        const response = await connection.execute(query);
+
+        res.redirect('/Users');
+    })
+    app.get("/delete-Users/:UserId",async function(req,res){
+        const {UserId} = req.params; // same as 'const UserId = req.params.UserId'
+        const query = `SELECT * FROM Users WHERE user_id =${UserId}`;
+        const [Users] = await connection.execute(query);
+        const userToDelete = Users[0];
+
+        res.render('delete-Users',{
+            'user': userToDelete
+        })
+
+    })
+    app.post('/delete-Users/:UserId',async function(req,res){
+        const {UserId} = req.params;
+        const query = `DELETE FROM Users WHERE user_id =${UserId}`;
+        const [Users]=await connection.execute(query);
+        res.redirect('/Users');
+
+    })
+    app.get('/update-Users/:UserId',async function(req,res){
+        const {UserId} = req.params;
+        const query = `SELECT * FROM Users WHERE user_id = ${req.params.UserId}`;
+        const [Users]=await connection.execute(query);
+        const wantedUser = Users[0];
+
+        res.redirect('update-customer');
+    })
+    
 }
 
 main();
---
+++ express-mysql/mock.sql
@@ -0,0 +1,2 @@
+INSERT INTO Users(name,email,address,contact_no)
+  VALUES ("Reji","reji.com","rejiMalelkunnil","59986976");
